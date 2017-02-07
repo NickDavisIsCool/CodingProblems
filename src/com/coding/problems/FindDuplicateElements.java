204f4342194862067a20eebb137ad119afc90a34
Nickolas Davis <Nickolas Davis>
2017-02-07 13:17:31 -0700

    created the element generation code

--- src/com/coding/problems/FindDuplicateElements.java
+++ src/com/coding/problems/FindDuplicateElements.java
@@ -1,10 +1,12 @@
 package com.coding.problems;
 
+import java.util.Random;
+
 public class FindDuplicateElements {
 
 	public static void main(String[] args) {
 		
-		int bounds = 100;
+		int bounds = 10;
 		int numDuplicates = 1;
 		
 		
@@ -12,11 +14,67 @@ public class FindDuplicateElements {
 
 	}
 	
+	/*
+	 * Generates a random uniformly distributed integer list with numbers between bounds and
+	 * numDuplicates.  Placement of values is based on the nearest available position
+	 * with respect to the initially randomly generated position value.
+	 * 
+	 */
 	public static Integer[] generateElements(int bounds, int numDuplicates){
 		Integer[] elements = new Integer[bounds];
+		Boolean[] available = new Boolean[bounds];
+		Random random = new Random();
+		int pos = 0;
+		int num = 0;
 		
+		for(int i = 0; i < available.length; i++){
+			available[i] = true;
+		}
 		
-		return null;
+		//Generate one occurrence of each number within bounds - numDuplicates
+		for(int i = 0; i < bounds - numDuplicates; i++){
+			pos = random.nextInt(bounds);
+			if(available[pos % bounds]){
+				elements[pos % bounds] = i;
+				available[pos % bounds] = false;
+			}
+			else{
+				while(true){
+					pos++;
+					if(available[pos % bounds]){
+						elements[pos % bounds] = i;
+						available[pos % bounds] = false;
+						break;
+					}
+				}
+			}	
+		}
+		
+		//Generate the extra numbers to fill the rest of the array
+		for(int i = 0; i < numDuplicates; i++){
+			num = random.nextInt(bounds - numDuplicates);
+			pos = random.nextInt(bounds);
+			if(available[pos % bounds]){
+				elements[pos % bounds] = num;
+				available[pos % bounds] = false;
+			}
+			else{
+				while(true){
+					pos++;
+					if(available[pos % bounds]){
+						elements[pos % bounds] = num;
+						available[pos % bounds] = false;
+						break;
+					}
+				}
+			}	
+		}
+		
+		for(int i = 0; i < bounds; i++){
+			System.out.println(elements[i]);
+		}
+
+		return elements;
 	}
 
 }
