16c67be10f12c964a4a3957271150b70d569734f
Nickolas Davis <Nickolas Davis>
2017-02-07 12:50:46 -0700

    started the FindDuplicateElements problem

---
+++ src/com/coding/problems/FindDuplicateElements.java
@@ -0,0 +1,22 @@
+package com.coding.problems;
+
+public class FindDuplicateElements {
+
+	public static void main(String[] args) {
+		
+		int bounds = 100;
+		int numDuplicates = 1;
+		
+		
+		Integer[] elements = generateElements(bounds, numDuplicates);
+
+	}
+	
+	public static Integer[] generateElements(int bounds, int numDuplicates){
+		Integer[] elements = new Integer[bounds];
+		
+		
+		return null;
+	}
+
+}
