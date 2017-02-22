78e844cef29a871b4191300324ec45933688f49b
Scenick-Panick <ndavis92x@gmail.com>
2017-02-22 13:03:34 -0700

    modified the MaxDirListingLength to not store strings, instead it does all the logic with simple addition and subtraction, and only iterates through the list of strings once and the whitespace before each string once

--- src/com/coding/problems/MaxDirListingLength.java
+++ src/com/coding/problems/MaxDirListingLength.java
@@ -12,13 +12,17 @@ public class MaxDirListingLength {
 		String S = "dir1\n dir11\n dir12\n  picture.jpeg\n  dir121\n  file1.txt\ndir2\n file2.gif";
 		String[] s = S.split("\n");
 		Stack<String> path = new Stack<>();
+		int pathLength = 0;
 		String string = null;
 		int maxLength = 0;
-		int currentDirectoryDepth = -1;
+        int previousLength = 0;
+        int currentDirectoryDepth = -1;
+
+		/*
 		for(int i = 0; i < s.length; i++){
 		    String concat = "/";
 		    int whitespace = 0;
-            int tempCounter = 1;
+            int tempCounter = 0;
             for(int j = 0; j < s[i].length(); j++){
                 if(s[i].charAt(j) == ' '){
                     whitespace++;
@@ -54,6 +58,40 @@ public class MaxDirListingLength {
             System.out.println(concat.substring(0, concat.length()-1));
 
 		}
+		*/
+
+        for(int i = 0; i < s.length; i++){
+            int whitespace = 0;
+            int currentLength = 0;
+
+            for(int j = 0; j < s[i].length(); j++){
+                if(s[i].charAt(j) == ' '){
+                    whitespace++;
+                } else {
+                    currentLength = s[i].trim().length();
+                    break;
+                }
+            }
+
+            if(s[i].endsWith(".jpeg") || s[i].endsWith(".gif") || s[i].endsWith(".jpg")){
+                if(pathLength > maxLength){
+                    maxLength = pathLength;
+                }
+            }
+
+            if(whitespace > currentDirectoryDepth){
+                pathLength = pathLength + currentLength + 1;
+                previousLength = currentLength + 1;
+                currentDirectoryDepth++;
+            } else if(whitespace == currentDirectoryDepth){
+                pathLength = pathLength + currentLength + 1 - previousLength;
+                previousLength = currentLength + 1;
+            } else {
+                pathLength = pathLength - previousLength;
+                currentDirectoryDepth--;
+            }
+
+        }
 
 		
 		System.out.println(maxLength);
